e65c0fac09de0bd0dbd32cf69117ade7e4d069e9
Gert Knibbe <gertknibbe03@gmail.com>
2026-07-11 21:49:17 +0200

    Who put this indent here

--- scripts/color-randomizer.js
+++ scripts/color-randomizer.js
@@ -1,14 +1,13 @@
-  const kleuren = [
-    '#e63946',
-    '#f1c40f',
-    '#16a085',
-    '#8e44ad',
-    '#2980b9',
-    '#e67e22',
-    '#2ecc71',
-    '#ff6b6b'
-  ];
+const kleuren = [
+  '#e63946',
+  '#f1c40f',
+  '#16a085',
+  '#8e44ad',
+  '#2980b9',
+  '#e67e22',
+  '#2ecc71',
+  '#ff6b6b'
+];
 
-  const randomKleur = kleuren[Math.floor(Math.random() * kleuren.length)];
-
-  document.documentElement.style.setProperty('--accent-kleur', randomKleur);
+const randomKleur = kleuren[Math.floor(Math.random() * kleuren.length)];
+document.documentElement.style.setProperty('--accent-kleur', randomKleur);
